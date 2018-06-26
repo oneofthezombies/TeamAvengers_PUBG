57aff70341a332cfec722f714aeb68e6e370e732
oneofthezombies <hunhoekim@gmail.com>
2018-06-27 05:19:56 +0900

    Added resource

---
+++ PUBG/Client/Source/CollidableStaticObject.cpp
@@ -0,0 +1,10 @@
+#include "stdafx.h"
+#include "CollidableStaticObject.h"
+
+void CollidableStaticObject::OnUpdate()
+{
+}
+
+void CollidableStaticObject::OnRender()
+{
+}
---
+++ PUBG/Client/Source/CollidableStaticObject.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "IObject.h"
+
+//class CollidableStaticObject : public IObject
+//{
+//private:
+//
+//public:
+//    CollidableStaticObject(const TAG_RESOU_STATIC)
+//
+//    virtual void OnUpdate() override;
+//    virtual void OnRender() override;
+//
+//};
