f3d908efcdc2bec991e91cb832927c4930577327
john han <johngamecoder@gmail.com>
2018-07-18 14:11:55 +0900

    working on blood particles

---
+++ PUBG/Client/BloodParticle.h
@@ -0,0 +1,61 @@
+#pragma once
+#include "IObject.h"
+
+struct VERTEX_PC
+{
+    D3DXVECTOR3 p;
+    D3DCOLOR c;
+
+    VERTEX_PC() {}
+    VERTEX_PC(D3DXVECTOR3 _p, D3DCOLOR _c) :p(_p), c(_c) {}
+
+    enum { FVF = D3DFVF_XYZ | D3DFVF_DIFFUSE };
+};
+
+class BloodParticle :
+    public IObject
+{
+    struct Blood_Hit
+    {
+        std::vector<LPDIRECT3DTEXTURE9>		    m_pTex;
+        std::vector<LPDIRECT3DVERTEXBUFFER9>    m_pVB;
+        int                                     m_currentIndex;
+        const int                               m_maxIndex;
+        
+
+        Blood_Hit(int maxIndex)
+            : m_maxIndex(maxIndex)
+            , m_currentIndex(0)
+        {
+        }
+
+    };
+
+private:
+    string m_fileName;//이미지 이름
+
+    std::vector<Blood_Hit>                      m_blood_hit;
+    D3DXVECTOR3                                 m_position;
+
+public:
+    BloodParticle();
+    ~BloodParticle();
+
+    void Init();
+    // Inherited via IObject
+    virtual void OnUpdate() override;
+    virtual void OnRender() override;
+
+    void setBloodImagePosition(D3DXVECTOR3& p)
+    {
+
+    }
+    void SetParticle(string filePath, Blood_Hit* blood_hit);
+    void renderBloodHit(Blood_Hit& blood_hit);
+    DWORD FtoDw(float f)
+    {
+        return *((DWORD*)&f);
+    }
+
+};
+
--- PUBG/Client/Source/Useful/TagClientOnly.h
+++ PUBG/Client/Source/Useful/TagClientOnly.h
@@ -9,6 +9,7 @@ enum class TAG_OBJECT
     Door,
     Window,
     Item,
+    Particle,
 };
 
 enum class TAG_COLLISION
