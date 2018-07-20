12e6c3d99d2e3bea03f88f0fffec9c35a14e77ad
john han <johngamecoder@gmail.com>
2018-07-20 21:51:11 +0900

    adding map from map tool complete

--- PUBG/Client/Source/Scene/SceneLoading.cpp
+++ PUBG/Client/Source/Scene/SceneLoading.cpp
@@ -166,9 +166,36 @@ void SceneLoading::Load()
     //load(TAG_RES_STATIC::ACOG);
 
     //load(TAG_RES_STATIC::Rock_1);
-    //load(TAG_RES_STATIC::WareHouse_A);
+    
 
-    //load(TAG_RES_STATIC::DeathDropBox);
+    //Map Buildings load
+    load(TAG_RES_STATIC::AbandonedTownHall);
+    load(TAG_RES_STATIC::Church);
+    load(TAG_RES_STATIC::Museum);
+    load(TAG_RES_STATIC::OldWoodenShed_1);
+    load(TAG_RES_STATIC::OldWoodenShed_2);
+    load(TAG_RES_STATIC::OldWoodenShed_3);
+    load(TAG_RES_STATIC::PoliceStation);
+    load(TAG_RES_STATIC::WareHouse_A);
+    load(TAG_RES_STATIC::WareHouse_B);
+
+    load(TAG_RES_STATIC::ContainerBox_A);
+    load(TAG_RES_STATIC::ContainerBox_B);
+    load(TAG_RES_STATIC::ContainerSmall_1);
+    load(TAG_RES_STATIC::ContainerSmall_2);
+
+    load(TAG_RES_STATIC::HayBale_1);
+    load(TAG_RES_STATIC::HayBale_2);
+
+    load(TAG_RES_STATIC::Silo_A);
+    load(TAG_RES_STATIC::Silo_B);
+    load(TAG_RES_STATIC::Lighthouse);
+
+    //Map buildings end
+
+
+
+    load(TAG_RES_STATIC::DeathDropBox);
 
     // load skined meshs
     const int numQBZ = 1;
